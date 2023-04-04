25ed22908fdc00d2ee2033eb204837e4cbe944a0
DummerTraumer <129345751+DummerTraumer@users.noreply.github.com>
2023-04-05 02:42:53 +0300

    исправленный код

--- snachala.java
+++ snachala.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.Objects;
 public class snachala {
     private static String convertNumToRoman (int numArabian) {
         String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
@@ -38,83 +39,100 @@ public class snachala {
                 return 10;
             }
         } catch (InputMismatchException e) {
-            throw new InputMismatchException("Неверный формат данных");
+            throw new InputMismatchException("Invalid data format");
         }
         return -1;
     }
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
+    public static String calc(String input){
         int a, b;
         int res = 0;
-        char op;
+        String op;
         boolean resaut = false;
         boolean r = false; 
-        String as, bs;
-        while(!resaut){
-            System.out.println("Введите первое число");
-            as = sc.next();
+        String as,bs;
+        String []arr=input.split(" ");
+        if (arr.length != 3){
+            System.out.println("The example is entered incorrectly. Enter in the format \"a sign b\"");
+            System.exit(0);
+        }
+        op=arr[1];
+        as=arr[0];
+        bs=arr[2];
+
             a= romanToNumber(as);
             if (a == -1) {
             a = Integer.parseInt (as);
             r = true;
             }
             if (a<0 || a >10) {
-               System.out.println("Исключение! Число не соответствует заданному дапазону");
-               continue;
+               System.out.println("An exception! The number does not match the specified range");
+               System.exit(0);
             }
-             System.out.println("Введите второе число");
-            bs = sc.next();
             b= romanToNumber(bs);
+
             if (b == -1 && r == true) {
-            b = Integer.parseInt (bs);
-            }
-            else 
-            {
-                System.out.println("Исключение! Числа находятся в разных системах исчесления");
-                continue;
-             }
-                if (b<0 || b>10) {
-                    System.out.println("Исключение! Число не соответствует заданному дапазону");
-                       continue;
-            }
-            if (b != -1 && r == false) {
                 b = Integer.parseInt (bs);
                 }
-                else 
+                else if(r==true||b==-1)
                 {
-                    System.out.println("Исключение! Числа находятся в разных системах исчесления");
-                    continue;
+                    System.out.println("An exception! The numbers are in different calculus systems");
+                    System.exit(0);
                  }
                     if (b<0 || b>10) {
-                        System.out.println("Исключение! Число не соответствует заданному дапазону");
-                           continue;
+                        System.out.println("An exception! The number does not match the specified range");
+                        System.exit(0);
                 }
-            System.out.print("Введите одну из (+, -, *, /): ");
-                op = sc.next(".").charAt(0);
-            if (op == '+'){
+            if (Objects.equals(op, new String("+"))){
                 res = a + b;
             }
-            else if (op == '-') {
+            else if (Objects.equals(op, new String("-"))) {
                 res = a - b;
             }
-            else if (op == '*') {
+            else if (Objects.equals(op, new String("*"))) {
                 res = a * b;
             }
-            else if (op == '/') {
+            else if (Objects.equals(op, new String("/"))) {
                 res = a / b;
             }
             else {
-                System.out.println("Исключение! Знак не соответствует заданному дапазону"); 
-                    continue;
+                System.out.println("An exception! The sign does not correspond to the specified range"); 
+                System. exit(0);
             }
             if (r == false) {
-                System.out.println(convertNumToRoman(res));
+                if (res<0)
+                 {
+                    System.out.println("An exception! The value of Roman numerals cannot be less than zero"); 
+                    System. exit(0);
+                    }
+                    return (convertNumToRoman(res));
             }
             else{
-            System.out.println((res));
+            return Integer.toString(res);
             }
-            resaut = true;
+
+
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        int res = 0;
+        String op;
+        boolean resaut = false;
+        boolean r = false; 
+        String as,bs;
+        if(args.length==3){
+            op=args[1];
+            as=args[0];
+            bs=args[2];
+            System.out.println(calc(as + " "+ op +" " + bs));
         }
+        else{
+
+            System.out.println("Enter an example");
+            System.out.println(calc(sc.nextLine()));
+        }
+ 
+   
         }
 }
